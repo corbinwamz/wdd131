675ec2bc7fa836e407152755e07c8f8017c9c9df
corbinwamz <corbinwa@byui.edu>
2026-01-29 10:14:59 -0700

    week 4 ponder 2

--- coolpics/photos.js
+++ coolpics/photos.js
@@ -2,4 +2,4 @@ const nav = document.querySelector("nav");
 const menu = document.querySelector("#menu");
 menu.addEventListener("click", function() {
     nav.classList.toggle("active");
-});
+});         
